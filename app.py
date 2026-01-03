a70478e24d64aa0406216f91e96d8ec9395fbc19
k_azuma <k.azuma@avanzare-tech.jp>
2026-01-03 19:07:29 +0900

    Webアプリ5終了

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,request, render_template
 
 app = Flask(__name__)
 
@@ -19,3 +19,25 @@ def profile(username):
 @app.route('/mypage')
 def mypage():
     return render_template('mypage.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['input_name']
+    age = request.form['input_age']
+    blood_type = request.form['input_blood_type']
+    birthday = request.form['input_birthday']
+    error = None
+
+    if not name or not age or not blood_type or not birthday:
+        error = '全ての項目を入力してください'
+    
+    return render_template('result.html', front_name=name, front_age=age, front_blood_type=blood_type,front_birthday=birthday,error=error)
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    return f'検索ワード: {query}'
